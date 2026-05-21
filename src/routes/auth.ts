97af66ccffdc84e80a68263e85b349470f072991
mchin24 <mchin24@gmail.com>
2026-05-21 17:03:39 -0400

    Added placeholder routes, returning 501 for now

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -5,37 +5,37 @@ const router = Router();
 
 router.get('/me', async (req, res) => {
     const userAccount = await getMe('54321');
-
-    res.status(200).send(userAccount);
-});
-
-/*
-router.get('/auth/me', async (req, res) => {
-    const user: UserAccount | null = await getMe(req.headers.authorization ?? "");
     if(user) {
         return res.status(200).json(user);
     } else {
         res.status(404).json({message: 'User not found'});
     }
+
+    res.status(200).send(userAccount);
 });
 
 router.post('/auth/register', async (req, res) => {
+    res.status(501);
 })
 
 router.post('/auth/login', async (req, res) => {
+    res.status(501);
 })
 
 router.post('/auth/logout', async (req, res) => {
+    res.status(501);
 })
 
 router.post('/auth/refresh', async (req, res) => {
+    res.status(501);
 })
 
 router.post('/auth/forgot-password', async (req, res) => {
+    res.status(501);
 })
 
 router.post('/auth/reset-password', async (req, res) => {
+    res.status(501);
 })
-*/
 
 export default router;
