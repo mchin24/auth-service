7187cd739a0b18b896789a98b4bd734a0b398af9
mchin24 <mchin24@gmail.com>
2026-05-26 15:01:08 -0400

    Implemented /auth/login endpoint

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,27 +1,18 @@
 import { Router } from 'express';
 import { requireAuth } from '../middleware/auth.js';
-import { register, getMe} from '../controllers/auth.js';
+import { register, login, getMe} from '../controllers/auth.js';
 
 const router = Router();
 
 router.get('/me', async (req, res) => {
     const userAccount = await getMe('54321');
-    /*
-    if(user) {
-        return res.status(200).json(user);
-    } else {
-        res.status(404).json({message: 'User not found'});
-    }
-     */
 
     res.status(200).send(userAccount);
 });
 
 router.post('/register', register)
 
-router.post('/auth/login', async (req, res) => {
-    res.status(501).send();
-})
+router.post('/login', login)
 
 router.post('/auth/logout', async (req, res) => {
     res.status(501).send();
